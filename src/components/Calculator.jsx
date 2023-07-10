e9632eac2c2dd5ff682fc757d53fa8591db0b519
sanditzz <kk.pon3@gmail.com>
2023-07-10 18:59:32 +0700

    Add Title

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,6 +4,7 @@ import '../App.css'
 export default function Calculator() {
   return (
     <div className='calculator-container min-h-screen bg-primary text-3xl text-white text-center flex flex-none flex-col items-center'>
+        <div className="calculator-title m-10">&#x2796; Calculator &#x2795;</div>
         <div className='calculator-grid grid grid-cols-[repeat(4,_6rem)] grid-rows-[minmax(7rem,_auto)_repeat(5,_6rem)] border rounded-2xl overflow-hidden'>
             <div className="output col-span-full bg-black/30 flex flex-col items-end justify-around p-3 break-all">
                 <div className="previous-operand text-white/90">123 *</div>
