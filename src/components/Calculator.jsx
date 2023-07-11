e7b9a7a3725600c18253362600fc98f2c8790220
sanditzz <kk.pon3@gmail.com>
2023-07-11 16:05:37 +0700

    Add reducer hook

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -140,7 +140,7 @@ export default function Calculator() {
   )
   
   return (
-    <div className='calculator-container min-h-screen bg-primary text-3xl text-white text-center flex flex-none flex-col items-center'>
+    <div className='Component-center'>
         <div className="calculator-title m-10">&#x2796; Calculator &#x2795;</div>
         <div className='calculator-grid grid grid-cols-[repeat(4,_6rem)] grid-rows-[minmax(7rem,_auto)_repeat(5,_6rem)] border rounded-2xl overflow-hidden'>
             <div className="output col-span-full bg-black/30 flex flex-col items-end justify-around p-3 break-all">
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,65 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 
 import '../App.css'
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, counterValue: state.counterValue + 1 }
+    case "DECREASE":
+      return { ...state, counterValue: state.counterValue -1 }
+    case "RESET":
+      return { ...state, counterValue: 0 }
+    case "CHANGE_VALUE_COLOR":
+      return { ...state, valueColor: action.payload }
+
+    default:
+      return state
+  }
+}
+
 export default function Counter () {
+  const [state, dispatch] = useReducer(reducer, {counterValue:0, valueColor: 'white'})
 
-    const [counterValue, setCounterValue] = useState(0)
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
 
-    const increment = () => {
-        setCounterValue(counterValue + 1)
+  const resetColor = (color) => {
+    dispatch ({ type: "CHANGE_VALUE_COLOR", payload: color })
   }
 
-    return (
-    <div className='Component-center'>
-        <div className='counter-title mb-5 text-4xl'>&#x1F9EE; Counter &#x1F9EE;</div>
-        <div className='counter-value text-5xl'>{counterValue}</div>
-        <button className='text-2xl m-5 select-none' onClick={increment}>Increase</button>
-    </div>
-  )
+  const increaseValueColor = () => {
+    const newValueColor = getRandomColor()
+    dispatch({ type: "INCREMENT"})
+    dispatch({ type:'CHANGE_VALUE_COLOR', payload: newValueColor})
+  }
+
+  const decreaseValueColor = () => {
+    const newValueColor = getRandomColor()
+    dispatch({ type: "DECREASE"})
+    dispatch({ type:'CHANGE_VALUE_COLOR', payload: newValueColor})
+  }
+
+  const resetValueColor = () => {
+    dispatch( { type: "RESET"})
+    resetColor('white');
+  }
+
+  return (
+  <div className='Component-center gap-5'>
+      <div className='counter-title mb-5 text-4xl'>&#x1F9EE; Counter &#x1F9EE;</div>
+      <div className='counter-value text-7xl' style={{color: state.valueColor}}>{state.counterValue}</div>
+      <div className='flex flex-row gap-0'>
+      <button onClick={decreaseValueColor}> Decrease <br /> &#x25C0;</button>
+      <button onClick={resetValueColor}>Reset<br /> &#x267B;</button>
+      <button onClick={increaseValueColor}>Increase <br /> &#x25B6; </button>
+      </div>
+  </div>
+)
 }
